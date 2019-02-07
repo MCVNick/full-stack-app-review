82cca2d2423dd470ebfdafa109517b0ef31c9d86
MCVNick <mcvnick@gmail.com>
2019-02-07 11:02:45 -0700

    +Login, Private components

---
+++ src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './Login.css'
+
+class Login extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="Login">
+                Login Component
+            </div>
+        )
+    }
+}
+
+export default Login
---
+++ src/components/Private/Private.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+import './Private.css'
+
+class Private extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="Private">
+                Private Component
+            </div>
+        )
+    }
+}
+
+export default Private
